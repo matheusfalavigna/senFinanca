252f25dc490ddef9c21a6b91ca1b78b618f740eb
matheus.falavigna <matheusfalavigna@gmail.com>
2023-08-14 11:22:54 -0300

    fix: correção da tora na baseURL

--- src/index.tsx
+++ src/index.tsx
@@ -17,25 +17,23 @@ export function makeServer({ environment = "development" } = {}) {
 
     routes() {
       if (environment === "development") {
-        this.namespace = "api";
-
-        this.get("/transactions", () => {
+        this.get("/api/transactions", () => {
           return this.schema.all("transaction");
         });
 
-        this.post("/transactions", (schema, request) => {
+        this.post("/api/transactions", (schema, request) => {
           const data = JSON.parse(request.requestBody);
 
           return schema.create("transaction", data);
         });
 
-        this.delete("/transactions/:id", (schema, request) => {
+        this.delete("/api/transactions/:id", (schema, request) => {
           const id = request.params.id;
           schema.db.transactions.remove(id);
           return new Response("", { status: 204 });
         });
 
-        this.put("/transactions/:id", (schema, request) => {
+        this.put("/api/transactions/:id", (schema, request) => {
           const id = request.params.id;
           const updatedTransaction = JSON.parse(request.requestBody);
           schema.db.transactions.update(id, updatedTransaction);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://senfinanca-chs2v0ijb-matheusfalavigna.vercel.app/",
+  baseURL: window.location.origin + "/api",
 });
