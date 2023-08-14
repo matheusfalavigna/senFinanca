bd455deff0adbcfc9d8d634ff13dc1f542d8c30a
matheus.falavigna <matheusfalavigna@gmail.com>
2023-08-14 11:18:23 -0300

    fix: correções da rota do deploy

--- src/index.tsx
+++ src/index.tsx
@@ -3,44 +3,50 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { Model, createServer } from "miragejs";
 
-createServer({
-  models: {
-    transaction: Model,
-  },
+export function makeServer({ environment = "development" } = {}) {
+  createServer({
+    models: {
+      transaction: Model,
+    },
 
-  seeds(server) {
-    server.db.loadData({
-      transactions: [],
-    });
-  },
+    seeds(server) {
+      server.db.loadData({
+        transactions: [],
+      });
+    },
 
-  routes() {
-    this.namespace = "api";
+    routes() {
+      if (environment === "development") {
+        this.namespace = "api";
 
-    this.get("/transactions", () => {
-      return this.schema.all("transaction");
-    });
+        this.get("/transactions", () => {
+          return this.schema.all("transaction");
+        });
 
-    this.post("/transactions", (schema, request) => {
-      const data = JSON.parse(request.requestBody);
+        this.post("/transactions", (schema, request) => {
+          const data = JSON.parse(request.requestBody);
 
-      return schema.create("transaction", data);
-    });
+          return schema.create("transaction", data);
+        });
 
-    this.delete("/transactions/:id", (schema, request) => {
-      const id = request.params.id;
-      schema.db.transactions.remove(id);
-      return new Response("", { status: 204 });
-    });
+        this.delete("/transactions/:id", (schema, request) => {
+          const id = request.params.id;
+          schema.db.transactions.remove(id);
+          return new Response("", { status: 204 });
+        });
 
-    this.put("/transactions/:id", (schema, request) => {
-      const id = request.params.id;
-      const updatedTransaction = JSON.parse(request.requestBody);
-      schema.db.transactions.update(id, updatedTransaction);
-      return new Response(updatedTransaction, { status: 200 });
-    });
-  },
-});
+        this.put("/transactions/:id", (schema, request) => {
+          const id = request.params.id;
+          const updatedTransaction = JSON.parse(request.requestBody);
+          schema.db.transactions.update(id, updatedTransaction);
+          return new Response(updatedTransaction, { status: 200 });
+        });
+      }
+    },
+  });
+}
+
+makeServer();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
