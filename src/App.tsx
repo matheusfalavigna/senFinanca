5baf20b657dde3f6cefe915623453fc29a20b76f
matheus.falavigna <matheusfalavigna@gmail.com>
2023-08-14 09:00:44 -0300

    feat: adicionando os components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-function App() {
+import { useState } from "react";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TrasanctionsProvider } from "./hook/useTransactions";
+import Modal from "react-modal";
+
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement("#root");
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+  const [editingTransaction, setEditingTransaction] = useState(null);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+    setEditingTransaction(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Typescript</h1>
-      </header>
-    </div>
+    <TrasanctionsProvider>
+      <Header />
+
+      <Dashboard onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+        editingTransaction={editingTransaction}
+      />
+
+      <GlobalStyle />
+    </TrasanctionsProvider>
   );
 }
-
-export default App;
